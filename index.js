153228147578e90f7ba92d361ef95c5aee610bc8
wtwerner <wtwerner@gmail.com>
2020-11-27 08:52:04 -0600

    commit tree 4844b9764164885b799a40b8f2e9390ba4ebb8bd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(arr, callback) {
+    let newArray = []
+    for (let i = 0; i < arr.length ; i++) {
+        newArray.push(callback(arr[i]))
+    }
+    return newArray
+}
+
+function reduce(arr, callback, init = 0) {
+    let agg = (!!init) ? init : arr[0]
+    let i = (!!init) ? 0 : 1
+    for (; i < arr.length; i++) {
+        agg = callback(arr[i], agg)
+    }
+    return agg
+} 
